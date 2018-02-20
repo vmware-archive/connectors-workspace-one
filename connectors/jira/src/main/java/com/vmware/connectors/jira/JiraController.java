e3d45d6a94819cd6829192e514058fc39921d25e
John Bard <jbard@vmware.com>
2018-02-20 08:26:24 -0500

    APF-811: Move Ticket Summary into description field
    Signed-off-by: John Bard <jbard@vmware.com>

--- connectors/jira/src/main/java/com/vmware/connectors/jira/JiraController.java
+++ connectors/jira/src/main/java/com/vmware/connectors/jira/JiraController.java
@@ -181,7 +181,7 @@ public class JiraController {
         CardAction.Builder openInActionBuilder = getOpenInActionBuilder(baseUrl, issueId);
 
         CardBody.Builder cardBodyBuilder = new CardBody.Builder()
-                .addField(buildGeneralBodyField("summary", summary))
+                .setDescription(summary)
                 .addField(buildGeneralBodyField("project", jiraResponse.read("$.fields.project.name")))
                 .addField(buildGeneralBodyField("components", String.join(",", components)))
                 .addField(buildGeneralBodyField("priority", jiraResponse.read("$.fields.priority.name")))
