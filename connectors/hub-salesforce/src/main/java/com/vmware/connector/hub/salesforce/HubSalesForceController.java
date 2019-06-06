bdd6766e146e9541c016d73ead6994a3f60bea90
Sudharsan Sathiamoorthy <ssathiamoort@vmware.com>
2019-06-06 15:24:52 +0530

    APF-2279-Fixed log statement

--- connectors/hub-salesforce/src/main/java/com/vmware/connector/hub/salesforce/HubSalesForceController.java
+++ connectors/hub-salesforce/src/main/java/com/vmware/connector/hub/salesforce/HubSalesForceController.java
@@ -218,7 +218,7 @@ public class HubSalesForceController {
 
         for (int i = 0; i < totalSize; i++) {
             if (workItemResponse.read(String.format("$.records[%s].Workitems", i)) == null) {
-                logger.trace("No work items found for the record id [{}]", (String) workItemResponse.read("$.records[0].Id"));
+                logger.trace("No work items found for the record id [{}]", (String) workItemResponse.read(String.format("$.records[%s].Id", i)));
                 continue;
             }
 
