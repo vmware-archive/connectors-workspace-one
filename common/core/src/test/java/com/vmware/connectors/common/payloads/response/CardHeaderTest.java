eecc79fa996c4d54a2e3a2103cda076a052b301f
Ryan Bard <jbard@vmware.com>
2019-09-17 09:39:05 -0400

    APF-2510: Add links to card builders
    Signed-off-by: Ryan Bard <jbard@vmware.com>

--- common/core/src/test/java/com/vmware/connectors/common/payloads/response/CardHeaderTest.java
+++ common/core/src/test/java/com/vmware/connectors/common/payloads/response/CardHeaderTest.java
@@ -19,9 +19,9 @@ class CardHeaderTest {
 
     @ParameterizedTest
     @MethodSource("hashTestArgProvider")
-    void hash(String t1, String t2, List<String> sub1, List<String> sub2, boolean shouldBeEqual) {
-        CardHeader h1 = new CardHeader(t1, sub1);
-        CardHeader h2 = new CardHeader(t2, sub2);
+    void hash(String t1, String t2, List<String> sub1, List<String> sub2, CardHeaderLinks links1, CardHeaderLinks links2, boolean shouldBeEqual) {
+        CardHeader h1 = new CardHeader(t1, sub1, links1);
+        CardHeader h2 = new CardHeader(t2, sub2, links2);
         if (shouldBeEqual) {
             assertEquals(h1.hash(), h2.hash());
         } else {
@@ -30,35 +30,39 @@ class CardHeaderTest {
     }
 
     private static Stream<Arguments> hashTestArgProvider() {
+        var links1 = new CardHeaderLinks("https://same.com/t", null);
+        var links2 = new CardHeaderLinks("https://same.com/t", null);
+        var links3 = new CardHeaderLinks("https://different.com/t", null);
         return Stream.of(
-                Arguments.of("t", "t", null, null, true),
-                Arguments.of("t", "t", List.of(), List.of(), true),
-                Arguments.of("t", "t", List.of("a"), List.of("a"), true),
-                Arguments.of("t", "t", List.of("a", "b"), List.of("a", "b"), true),
-                Arguments.of("t", "t", List.of("a", "b", "c"), List.of("a", "b", "c"), true),
-                Arguments.of("", "", List.of("a", "b", "c"), List.of("a", "b", "c"), true),
-                Arguments.of("t", "t", List.of("a", "", "c"), List.of("a", "", "c"), true),
-                Arguments.of("t", "t", List.of("", "", ""), List.of("", "", ""), true),
+                Arguments.of("t", "t", null, null, null, null, true),
+                Arguments.of("t", "t", List.of(), List.of(), links1, links2, true),
+                Arguments.of("t", "t", List.of("a"), List.of("a"), links1, links2, true),
+                Arguments.of("t", "t", List.of("a", "b"), List.of("a", "b"), links1, links2, true),
+                Arguments.of("t", "t", List.of("a", "b", "c"), List.of("a", "b", "c"), links1, links2, true),
+                Arguments.of("", "", List.of("a", "b", "c"), List.of("a", "b", "c"), links1, links2, true),
+                Arguments.of("t", "t", List.of("a", "", "c"), List.of("a", "", "c"), links1, links2, true),
+                Arguments.of("t", "t", List.of("", "", ""), List.of("", "", ""), links1, links2, true),
                 // simple differences
-                Arguments.of("t", "t", null, List.of(), false),
-                Arguments.of("t", "X", List.of("a", "b", "c"), List.of("a", "b", "c"), false),
-                Arguments.of("t", "t", List.of("X", "b", "c"), List.of("a", "b", "c"), false),
-                Arguments.of("t", "t", List.of("a", "X", "c"), List.of("a", "b", "c"), false),
-                Arguments.of("t", "t", List.of("a", "b", "X"), List.of("a", "b", "c"), false),
+                Arguments.of("t", "t", null, List.of(), links1, links2, false),
+                Arguments.of("t", "X", List.of("a", "b", "c"), List.of("a", "b", "c"), links1, links2, false),
+                Arguments.of("t", "t", List.of("X", "b", "c"), List.of("a", "b", "c"), links1, links2, false),
+                Arguments.of("t", "t", List.of("a", "X", "c"), List.of("a", "b", "c"), links1, links2, false),
+                Arguments.of("t", "t", List.of("a", "b", "X"), List.of("a", "b", "c"), links1, links2, false),
+                Arguments.of("t", "t", List.of("a", "b", "c"), List.of("a", "b", "c"), links1, links3, false),
                 // tricky differences
                 // make sure it doesn't fully rely on List's toString
-                Arguments.of("t", "t", List.of("a,b", "c"), List.of("a", "b", "c"), false),
-                Arguments.of("t", "t", List.of("a, b", "c"), List.of("a", "b", "c"), false),
+                Arguments.of("t", "t", List.of("a,b", "c"), List.of("a", "b", "c"), links1, links2, false),
+                Arguments.of("t", "t", List.of("a, b", "c"), List.of("a", "b", "c"), links1, links2, false),
                 // make sure it doesn't fully rely on separators
-                Arguments.of("t", "t", List.of("a|b", "c"), List.of("a", "b", "c"), false),
-                Arguments.of("t", "t", List.of("a | b", "c"), List.of("a", "b", "c"), false),
-                Arguments.of("t", "t", List.of("a;b", "c"), List.of("a", "b", "c"), false),
-                Arguments.of("t", "t", List.of("a; b", "c"), List.of("a", "b", "c"), false),
-                Arguments.of("t", "t", List.of("a ", "b c"), List.of("a", "b", "c"), false),
+                Arguments.of("t", "t", List.of("a|b", "c"), List.of("a", "b", "c"), links1, links2, false),
+                Arguments.of("t", "t", List.of("a | b", "c"), List.of("a", "b", "c"), links1, links2, false),
+                Arguments.of("t", "t", List.of("a;b", "c"), List.of("a", "b", "c"), links1, links2, false),
+                Arguments.of("t", "t", List.of("a; b", "c"), List.of("a", "b", "c"), links1, links2, false),
+                Arguments.of("t", "t", List.of("a ", "b c"), List.of("a", "b", "c"), links1, links2, false),
                 // make sure it doesn't rely on space separator and trimming the input
-                Arguments.of("t", "t", List.of("a  b", "", "c"), List.of("a", "", "b  c"), false),
+                Arguments.of("t", "t", List.of("a  b", "", "c"), List.of("a", "", "b  c"), links1, links2, false),
                 // make sure it handles different size empty content
-                Arguments.of("t", "t", List.of("", ""), List.of("", "", ""), false)
+                Arguments.of("t", "t", List.of("", ""), List.of("", "", ""), links1, links2, false)
         );
     }
 
